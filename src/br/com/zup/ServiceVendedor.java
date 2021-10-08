8dc12f511b6a1ca6a42deffbb29f303d57d7eb88
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-08 11:30:55 -0300

    Criar lista de vendedores

--- src/br/com/zup/ServiceVendedor.java
+++ src/br/com/zup/ServiceVendedor.java
@@ -1,4 +1,11 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServiceVendedor {
+
+    private static List<Vendedor> listaDeVendedor = new ArrayList<>();
+
+
 }
