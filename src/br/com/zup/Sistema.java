89552f9e9b4779cf583127be67126854a5a2f0e9
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-08 10:56:19 -0300

    Criar método cadastrarVenda no Sistema

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -3,8 +3,16 @@ package br.com.zup;
 import java.util.Scanner;
 
 public class Sistema {
+
     private static Scanner capturarDados (String mensagem){
         System.out.println(mensagem);
         return new Scanner(System.in);
+    }
+
+    public static Venda cadastrarVenda(){
+        String emailCliente = capturarDados("Digite o email do cliente que deseja cadastrar esta venda").nextLine();
+        String emailVendedor = capturarDados("Digite o email do vendedor que realizou a venda").nextLine();
+        double valor = capturarDados("Qual é o valor desta venda").nextDouble();
+        String data = capturarDados("Qual é a data do registro").nextLine();
     }
 }
