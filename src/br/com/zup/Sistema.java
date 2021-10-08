3870eaff51f9c175218c2f5b58c9b5e1c1f44159
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-08 10:49:30 -0300

    Criar método capturarDados

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,4 +1,10 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Sistema {
+    private static Scanner capturarDados (String mensagem){
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
 }
