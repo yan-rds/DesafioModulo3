8c21eb3c37e68e194c71c869d1801719ac13c5c0
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-08 11:08:59 -0300

    Criar lista de clientes

--- src/br/com/zup/ServiceCliente.java
+++ src/br/com/zup/ServiceCliente.java
@@ -1,4 +1,12 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServiceCliente {
+
+    private static List<Cliente> listaDeClientes = new ArrayList<>();
+
+
+
 }
