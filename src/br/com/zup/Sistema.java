801f1311cd12fd6eab24094a124d70f23e08e58d
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-08 11:03:08 -0300

    Criar método cadastrarCliente no Sistema

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -15,4 +15,10 @@ public class Sistema {
         double valor = capturarDados("Qual é o valor desta venda").nextDouble();
         String data = capturarDados("Qual é a data do registro").nextLine();
     }
+
+    public static Cliente cadastrarCliente(){
+        String nome = capturarDados("Qual é o nome do cliente?").nextLine();
+        String cpf = capturarDados(("Qual é o CPF do cliente:")).nextLine();
+        String email = capturarDados("Qual é o email do cliente?").nextLine();
+    }
 }
