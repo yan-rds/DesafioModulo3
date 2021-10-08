b7ddcaed78df6697ba7dd40d47968c4dca56f86f
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-08 11:00:39 -0300

    Criar molde do método CadastrarVenda no ServiceVenda

--- src/br/com/zup/ServiceVenda.java
+++ src/br/com/zup/ServiceVenda.java
@@ -5,4 +5,12 @@ import java.util.List;
 
 public class ServiceVenda {
     private static List<Venda> listaDeVendas = new ArrayList<>();
+
+    public static Venda cadastrarVenda (String emailCliente, String emailVendedor, double Valor, String data){
+        // Verificar emailCliente e retornar um cliente
+        // Verificar emailVendedor e retornar um vendedor
+        // Instanciar uma venda com essas informações
+        // Adicionar a venda à lista de vendas
+        // Retornar Venda
+    }
 }
