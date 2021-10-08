b8e44787c7c287804e1faad5ab117a1da4dae355
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-08 11:10:49 -0300

    Criar método cadastrarCliente no ServiceCliente

--- src/br/com/zup/ServiceCliente.java
+++ src/br/com/zup/ServiceCliente.java
@@ -7,6 +7,10 @@ public class ServiceCliente {
 
     private static List<Cliente> listaDeClientes = new ArrayList<>();
 
-
+    public static Cliente cadastrarCliente (String nome, String cpf, String email){
+        Cliente cliente = new Cliente(nome, cpf, email);
+        listaDeClientes.add(cliente);
+        return cliente;
+    }
 
 }
