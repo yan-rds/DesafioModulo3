8891645f457987f43a599777b308388315fd2b2e
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-09 20:29:05 -0300

    Criar molde do método validarCpf

--- src/br/com/zup/Validadores.java
+++ src/br/com/zup/Validadores.java
@@ -1,4 +1,11 @@
 package br.com.zup;
 
 public class Validadores {
+
+    public static void validarCpf (String cpf){
+        // Receber o CPF
+        // Transformar os caracteres do cpf em Int
+        // Definir os dígitos verificadores para esse cpf
+        // Comparar se os digitos verificadores são iguais ao dado pelo usuario
+    }
 }
