d0e92614e5faf8d22aab49ba866e86e400120349
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-09 20:44:38 -0300

    Definir os dígitos verificadores corretos para o CPF

--- src/br/com/zup/Validadores.java
+++ src/br/com/zup/Validadores.java
@@ -5,6 +5,7 @@ public class Validadores {
     public static void validarCpf (String cpf) throws Exception{
         // Receber o CPF
         String cpfRecebido = cpf;
+
         // Recusar CPFs impossíveis
         if (cpf.equals("00000000000") ||
             cpf.equals("11111111111") ||
@@ -19,8 +20,9 @@ public class Validadores {
             cpf.length() != 11){
             throw new Exception("CPF inválido");
         }
+
         // Transformar os caracteres do cpf em Int
-        String S1, S2, S3, S4, S5, S6, S7, S8, S9, confere = cpf;
+        String S1, S2, S3, S4, S5, S6, S7, S8, S9, cpfIdeal = cpf;
         int N1, N2, N3, N4, N5, N6, N7, N8, N9, verificador1, verificador2;
 
         S1 = cpf.substring(0, 1); N1 = Integer.parseInt(S1);
@@ -34,6 +36,22 @@ public class Validadores {
         S9 = cpf.substring(8, 9); N9 = Integer.parseInt(S9);
 
         // Definir os dígitos verificadores para esse cpf
+        verificador1 = (N1 * 10 + N2 * 9 + N3 * 8 + N4 * 7 + N5 * 6 + N6 * 5 + N7 * 4 + N8 * 3 + N9 * 2);
+        if ((verificador1 % 11) < 2){
+            verificador1 = 0;
+        }
+        else {
+            verificador1 = 11 - (verificador1 % 11);
+        }
+
+        verificador2 = (N1 * 11 + N2 * 10 + N3 * 9 + N4 * 8 + N5 * 7 + N6 * 6 + N7 * 5 + N8 * 4 + N9 * 3 + verificador1 * 2);
+        if ((verificador2 % 11) < 2){
+            verificador2 = 0;
+        }
+        else {
+            verificador2 = 11 - (verificador2 % 11);
+        }
+
         // Comparar se os digitos verificadores são iguais ao dado pelo usuario
     }
 }
