9b3fb9f73d762c4e8a80640f20f17893aed92e92
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-09 20:32:51 -0300

    Recusar CPFs impossíveis

--- src/br/com/zup/Validadores.java
+++ src/br/com/zup/Validadores.java
@@ -2,9 +2,23 @@ package br.com.zup;
 
 public class Validadores {
 
-    public static void validarCpf (String cpf){
+    public static void validarCpf (String cpf) throws Exception{
         // Receber o CPF
+        String cpfRecebido = cpf;
         // Recusar CPFs impossíveis
+        if (cpf.equals("00000000000") ||
+            cpf.equals("11111111111") ||
+            cpf.equals("22222222222") ||
+            cpf.equals("33333333333") ||
+            cpf.equals("44444444444") ||
+            cpf.equals("55555555555") ||
+            cpf.equals("66666666666") ||
+            cpf.equals("77777777777") ||
+            cpf.equals("88888888888") ||
+            cpf.equals("99999999999") ||
+            cpf.length() != 11){
+            throw new Exception("CPF inválido");
+        }
         // Transformar os caracteres do cpf em Int
         // Definir os dígitos verificadores para esse cpf
         // Comparar se os digitos verificadores são iguais ao dado pelo usuario
