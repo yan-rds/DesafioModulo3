7626ca74354552ffefe139fa280e1a2f2840912a
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-10-09 20:30:29 -0300

    Atualizar molde do método validarCpf

--- src/br/com/zup/Validadores.java
+++ src/br/com/zup/Validadores.java
@@ -4,6 +4,7 @@ public class Validadores {
 
     public static void validarCpf (String cpf){
         // Receber o CPF
+        // Recusar CPFs impossíveis
         // Transformar os caracteres do cpf em Int
         // Definir os dígitos verificadores para esse cpf
         // Comparar se os digitos verificadores são iguais ao dado pelo usuario
